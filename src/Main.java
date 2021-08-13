104daa93492717012e5b1f815ffb1088b4208e90
Kristina Semenova <semenova.kristina.00@mail.ru>
2021-08-13 10:25:03 +0300

    Added Main.java and report.md

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int start = 2000000000;
+        int added = 500000000;
+        int total = start + added;
+        System.out.println(total);;
+    }
 }
